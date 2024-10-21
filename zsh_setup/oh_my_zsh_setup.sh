588e48c690ed38866c1aa41a379c0a9366f115ab
Thiago M <rpj134@gmail.com>
2024-10-21 12:51:51 -0300

    Fix Oh My Zsh: instalação funcionando

--- zsh_setup/oh_my_zsh_setup.sh
+++ zsh_setup/oh_my_zsh_setup.sh
@@ -1,11 +1,29 @@
-#Instalar Oh my zsh (TERMINAR)
+#Instalar Oh my zsh
+    folder_path="$(find ~/ -name "my-setup")"/zsh_setup
+
+    # Pré Requisitos
     sudo apt update -y
     sudo apt install zsh curl -y
 
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+    # Caso já exista não instala o Oh my zsh
+    if [ -n "$(ls -a ~/ | grep .oh-my-zsh)" ]; then
+        echo "OH-MY-ZSH Já está instalado"
+    else
+        # Download e instalação
+        wget -P "$folder_path" https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+        # Comentar a linha de inicialização automática para não travar o resto da instalação
+        sed -i '/exec zsh -l/s/^/#/' "$folder_path"/install.sh
+        sh "$folder_path"/install.sh
+        rm -f "$folder_path"/install.sh
+    fi
+    
+    # Configuração de plugins do zsh
     git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     sudo apt install zoxide
-
+    
+    # Passando minhas configurações do zsh
     sudo rm ~/.zshrc
-    cp zshsetup.txt ~/.zshrc
+    cp "$folder_path"/zshsetup.txt ~/.zshrc
+
+    # Definindo o zsh como padrão
+    chsh -s $(which zsh)
