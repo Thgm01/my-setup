61b4b94e3e2dcb68694a814afce1ad64659ece70
Thiago M <rpj134@gmail.com>
2024-10-26 17:48:16 -0300

    Update instalação Kicad: Verificando se ja tem o kicade no repositório do apt

--- kicad_install.sh
+++ kicad_install.sh
@@ -1,4 +1,9 @@
 # Instalação do Kicad 8.0
-sudo add-apt-repository ppa:kicad/kicad-8.0-releases
-sudo apt update
-sudo apt install kicad
+if [ -z "$(grep -r ^deb /etc/apt/sources.list* | grep kicad)" ]; then
+  sudo add-apt-repository ppa:kicad/kicad-8.0-releases
+else
+  printf "\nJá está posuí  o repositorio no apt!\n\n"
+fi
+
+sudo apt update -y
+sudo apt install kicad -y
