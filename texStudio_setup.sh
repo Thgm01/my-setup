16f2edf7f5beeafe6fbe0f4878be7c9c5997a637
Thiago M <rpj134@gmail.com>
2024-11-03 12:34:07 -0300

    Add Instalação do tex studio

---
+++ texStudio_setup.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+
+# Quando a instalação travar aperte e segure o botão enter por um tempo
+# que a instalação irá continuar (Não faço ideia o porque mas funciona)
+sudo apt install texstudio texlive-full -y
+
+
