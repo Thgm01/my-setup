e79b9be9300ec0389a8f49af84e6e5ed87d2f08f
Thiago M <rpj134@gmail.com>
2025-08-22 22:47:12 -0300

    Add zsh syntax sugestion && Change zsh theme to agnoster

--- zsh_setup/oh_my_zsh_setup.sh
+++ zsh_setup/oh_my_zsh_setup.sh
@@ -19,6 +19,7 @@
     
     # Configuração de plugins do zsh
     git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     sudo apt install zoxide
     
     # Passando minhas configurações do zsh
